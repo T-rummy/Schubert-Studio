1fb8abb19ab673072077f23c928933dfbc3cf729
Ruminert <ruminertt@Tanners-MacBook-Air.local>
2022-08-14 20:49:46 -0600

    hide nav at small, images wider

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// var bigNav = document.querySelector(".big-nav");
+// var anchor = document.querySelector(".big-anc2");
+
+
+// const screenSize = () => {
+// if (screen.width < 767) {
+
+//     bigNav.setAttribute("class", "sm-nav");
+
+// }
+// }
+
+// screenSize();
