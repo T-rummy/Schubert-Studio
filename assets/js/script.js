34d9f774f7c1072ed8a86c8ad7a4ac2f90b5f6c3
Ruminert <ruminertt@Tanners-MacBook-Air.local>
2022-08-18 20:22:49 -0500

    loading page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
+var myVar;
 
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
 
 function openNav() {
   document.getElementById("myNav").style.height = "100%";
